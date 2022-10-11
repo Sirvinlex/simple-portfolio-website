185750ce30ddc135d51b726cfbac4ed7d96f77e9
sirvinlex <vinlex3030@gmail.com>
2022-10-11 11:07:02 +0100

    updating starting files

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MdEmail } from 'react-icons/md'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
