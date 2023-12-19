34b1066513d5cb63ba2af98943d570a310a0027b
Alex <vinlex3030@gmail.com>
2023-12-18 20:38:10 -0800

    updated project list

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,8 +18,13 @@ const Body = () => {
             <h3 className='project-title'>Projects</h3>
             <hr className='project-hr' />
             <div className='projects'>
+               <p><a href='https://book-trading-club.netlify.app/' target='_blank' rel="noopener noreferrer">Book Trading App</a></p>
+               <p><a href='https://vinlex-task-manager.netlify.app/' target='_blank' rel="noopener noreferrer">Task Manager</a></p>
+               <p><a href='https://sirvinlex-recipe-box.netlify.app/' target='_blank' rel="noopener noreferrer">Recipe Box</a></p>
                <p><a href='https://tik-video-app.netlify.app/' target='_blank' rel="noopener noreferrer">Tik-Vid video sharing app</a></p>
                <p><a href='https://job-checker.netlify.app/' target='_blank' rel="noopener noreferrer">Job Checker</a></p>
+               <p><a href='https://codepen.io/Sirvinlex/full/vYvVgwy' target='_blank' rel="noopener noreferrer">Wikipedia Viewer</a></p>
+               <p><a href='https://roman-numeral-generator.netlify.app/' target='_blank' rel="noopener noreferrer">Roman Numeral Converter</a></p>
                <p><a href='https://tca-fitness-gym.netlify.app/' target='_blank' rel="noopener noreferrer">Fitness app</a></p>
                <p><a href='https://vinlex-admin-dashboard-template.netlify.app/' target='_blank' rel="noopener noreferrer">Admin dashboard template</a></p>
                <p><a href='https://vinlex-restaurant-landing-page.netlify.app/' target='_blank' rel="noopener noreferrer">Restaurant landing page</a></p>
